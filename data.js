0871df9095c23d0075dad6ab64ab187aa5b85e2f
adhayes1906-coder <adhayes1906@gmail.com>
2026-06-17 18:49:47 -0400

    Create data.js
    Adding new data file

---
+++ data.js
@@ -0,0 +1,73 @@
+const DATA = {
+
+NYC: {
+
+1: `
+Arrive at hotel
+https://maps.google.com?q=Times+Square
+
+Explore Times Square
+`,
+
+2: `
+Walk to Times Sq subway
+https://maps.google.com?q=Times+Square+42+St
+
+Take 4 or 5 Train Downtown
+https://maps.google.com?q=Brooklyn+Bridge
+
+Walk Brooklyn Bridge
+https://maps.google.com?q=Brooklyn+Bridge
+
+Explore DUMBO
+https://maps.google.com?q=DUMBO+Brooklyn
+
+Hamilton Broadway Show
+https://maps.google.com?q=Richard+Rodgers+Theatre
+`,
+
+3: `
+Take 1 Train Downtown
+https://maps.google.com?q=South+Ferry
+
+Statue of Liberty
+https://maps.google.com?q=Statue+of+Liberty
+
+9/11 Memorial
+https://maps.google.com?q=9/11+Memorial
+`,
+
+4: `
+Empire State Building
+https://maps.google.com?q=Empire+State+Building
+
+Take Subway to Harlem
+https://maps.google.com?q=125+Street+Harlem
+
+Juneteenth Festival Harlem
+https://maps.google.com?q=Harlem
+`,
+
+5: `
+Central Park Zoo
+https://maps.google.com?q=Central+Park+Zoo
+
+Go to JFK Airport
+https://maps.google.com?q=JFK+Airport
+`
+
+},
+
+CRUISE: {
+
+1: `Drive to Galveston and Board Ship`,
+2: `Sea Day`,
+3: `Costa Maya`,
+4: `Roatan Snorkel Safari`,
+5: `Cozumel`,
+6: `Sea Day`,
+7: `Return to Galveston`
+
+}
+
+};
